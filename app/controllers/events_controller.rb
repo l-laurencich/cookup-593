1c285db9e8877d38d838f0ca9eb655d220497458
XueyingHou <houxueyingmei@gmail.com>
2021-05-25 16:28:02 +0200

    validated the price and capacity,finished new and create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,18 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    @event.save
+    if @event.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +27,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start, :end, :name, :menu, :location, :price, :capacity)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,6 @@ class Event < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :start, :end, :name, :menu, :location, :price, :capacity, presence: true
-  validates :name, length: { minimum: 6}
+  validates :name, length: { minimum: 6 }
+  validates :price, :capacity, numericality: { greater_than_or_equal_to: 0 }
 end
