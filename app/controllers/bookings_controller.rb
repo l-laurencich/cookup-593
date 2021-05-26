b8ea0cf0cd0a88fca66a80f4bf0c91b8e84c911b
Francesca Fenu <fenufrancesca@yahoo.de>
2021-05-26 12:17:50 +0200

    basic bookings controller new/edit & show on event page

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  def new
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new(booking_params)
+    @booking.event = @event
+    @booking.user = current_user
+    if @booking.save
+      redirect_to event_path(@event)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:message, :event_id, :number_of_guests)
+  end
+end
