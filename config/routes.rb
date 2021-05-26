3711a53ec9835e1e3eccb346d4ed0b2e277b1745
Francesca Fenu <fenufrancesca@yahoo.de>
2021-05-26 13:04:46 +0200

    small frontend changes to bookings & destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :events do
     resources :bookings, only: [ :new, :create ]
   end
+  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
